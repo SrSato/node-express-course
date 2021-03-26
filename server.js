cc97dc63aa9d134e416f2d2ace0086d92acc02e9
Sera <sera.martin@gmail.com>
2021-03-26 12:53:09 +0100

    Añadimos una ruta GET en /users

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const datosDePega=[
+{name:'Sera'},
+{name:'Tunante'}
+]
+
+app.get('/users',function(req,res){
+	res.json({
+		success:true,
+		message: 'Recogemos correctametne los usuarios. Moooola!',
+		users:datosDePega
+	})
+})
+
 app.listen(8000,function(){
 	console.log("Servidor furrulando!")
 })
