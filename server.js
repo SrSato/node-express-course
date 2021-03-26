1b28ae4ce8849753cf9b413ef9abb2d920a19d52
Sera <sera.martin@gmail.com>
2021-03-26 13:31:50 +0100

    Añadimos una ruta POST para el login

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');
+//Necesidades iniciales
+const express = require('express'); //lo necesitamos para que funcione express
 const app = express();
+const bodyParser = require('body-parser'); //Lo necesitamos para las rutas post -incrusta en el body del req-
 
+app.use(bodyParser.json()); //Habilitamos el uso de body parser para nuestra app. Pa las rutas POST ;)
+
+//Nuestras mandangas. Esto ya no son necesidades iniciales.
 const datosDePega=[
 {name:'Sera'},
 {name:'Tunante'}
@@ -15,6 +20,7 @@ app.get('/users',function(req,res){
 	})
 })
 
+//Ruta GET dinámica para pillar datos de cada usuario
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -22,6 +28,28 @@ app.get('/users/:id',function(req,res){
 		message: 'Pescamos un usuario!',
 		usr: req.params.id
 	})
+})
+
+//Ruta POST para fingir un login
+app.post('/login',function(req,res){
+	const username=req.body.username;
+	const password=req.body.password;
+
+	const nickDePega="BichoMalo";
+	const pwdDePega="noTeLoDigo";
+
+	if(username===nickDePega && password===pwdDePega){
+		res.json({
+			success:true,
+			message: 'Bienvenido y padentro!',
+			token: 'Aquí iría un token encriptadito'
+		})
+	} else {
+		res.json({
+			success:false,
+			message: 'Pasa algo con ese password o ese usuario... Pa mi que no son buenos'
+		})
+	}
 })
 
 //Levantamos el server
